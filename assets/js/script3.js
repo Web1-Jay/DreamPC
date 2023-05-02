5b83263d189e1b3449a90c8ce782c5eb1ecf5c42
Web1-Jay <vaughnj1@tcnj.edu>
2023-05-02 16:59:41 -0400

    work

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -11,3 +11,14 @@ for (i = 0; i < faq.length; i++) {
         }
     });
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
