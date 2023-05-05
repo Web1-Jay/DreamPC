3d671ff88c99ffa260408fb0803301c411a32f83
Web1-Jay <vaughnj1@tcnj.edu>
2023-05-05 00:59:43 -0400

    almost done

---
+++ assets/js/script2.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
